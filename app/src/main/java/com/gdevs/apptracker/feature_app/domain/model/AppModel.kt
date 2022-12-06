802b406e779a42e125a77cd3df4a1ef0bc012ef1
Gustavo García Salas <gggustavogs@gmail.com>
2022-12-06 12:17:23 -0600

    Check for permissions

--- app/src/main/java/com/gdevs/apptracker/feature_app/domain/model/AppModel.kt
+++ app/src/main/java/com/gdevs/apptracker/feature_app/domain/model/AppModel.kt
@@ -1,7 +1,9 @@
 package com.gdevs.apptracker.feature_app.domain.model
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+@Entity
 data class AppModel(
     val appName: String,
     val firstTimeStamp: String,
--- app/src/main/java/com/gdevs/apptracker/presentation/MainActivity.kt
+++ app/src/main/java/com/gdevs/apptracker/presentation/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.gdevs.apptracker.presentation
 
+import android.app.AppOpsManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.os.Process
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,6 +15,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.AppOpsManagerCompat
+import androidx.core.content.ContextCompat.startActivity
 import com.gdevs.apptracker.ui.theme.AppTrackerTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,6 +30,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     Greeting("Android")
+                    requestPermissions(this)
                 }
             }
         }
@@ -40,4 +48,22 @@ fun DefaultPreview() {
     AppTrackerTheme {
         Greeting("Android")
     }
+}
+
+private fun requestPermissions(context: Context): Boolean {
+    val appOpsManager: AppOpsManager =
+        context.getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
+    //val mode = appOpsManager.unsafeCheckOpNoThrow(OPSTR_GET_USAGE_STATS, Process.myUid(), context.packageName)
+    val mode = appOpsManager.checkOpNoThrow(
+        AppOpsManager.OPSTR_GET_USAGE_STATS,
+        Process.myUid(),
+        context.packageName
+    )
+
+    if (mode != AppOpsManagerCompat.MODE_ALLOWED) {
+        //TODO CREATE DIALOG TO OPEN SETTINGS AND GIVE PERMISSION
+        context.startActivity(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS))
+        println("TIME PLEASE GIVE PERMISSION")
+    }
+    return mode == AppOpsManagerCompat.MODE_ALLOWED
 }
